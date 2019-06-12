dee6c88391d63f480ebd423903a06fbf4ffe5183
Karol Jagodziński <karoljago@gmail.com>
2019-06-12 18:41:45 +0200

    [43] Lint changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,14 +3,12 @@ import styles from "./Layout.scss";
 import Header from "./Header/Header";
 import Dashboard from "./Dashboard/Dashboard";
 import Sidebar from "./Sidebar/Sidebar";
-import SaveButton from "./SaveButton/SaveButton";
 
 const Layout = () => {
   return (
     <div className={styles.Layout}>
       <Header />
       <Sidebar />
-      {/*<SaveButton />*/}
       <Dashboard />
     </div>
   );
