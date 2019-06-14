65b3e4347f4a5e88b45f6536fc9dc940e2d95d54
Karol Jagodziński <karoljago@gmail.com>
2019-06-14 11:27:55 +0200

    [43] Change class compontents to functional (#44)
    * [43] Change class compontents to functional

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Dashboard.scss";
 
-const Dashboard = () => (
-  <div className={styles.container}>
-    <p>Dashboard</p>
-    <div className={styles.wrapper}>test</div>
-  </div>
-);
+const Dashboard = () => {
+  return (
+    <div className={styles.Dashboard}>
+      <p>Dashboard</p>
+      <div className={styles.Wrapper}>test</div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./Layout.scss";
 import Header from "./Header/Header";
 import Dashboard from "./Dashboard/Dashboard";
 import Sidebar from "./Sidebar/Sidebar";
-// import SaveButton from "./SaveButton/SaveButton";
 
-class Layout extends Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        <Header />
-        <Sidebar />
-        {/* <SaveButton /> */}
-        <Dashboard />
-      </div>
-    );
-  }
-}
+const Layout = () => {
+  return (
+    <div className={styles.Layout}>
+      <Header />
+      <Sidebar />
+      <Dashboard />
+    </div>
+  );
+};
 
 export default Layout;
