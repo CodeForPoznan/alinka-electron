4d6c626f5e5e2987b3c773499bf42b10fa95257e
kimdil89 <kimdil1989@gmail.com>
2019-08-26 21:57:12 +0200

    dropdown component

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Dropdown from 'react-dropdown';
+import styles from './Dropdown.scss';
+
+const options = [
+  'szkola1', 'szkola2', 'szkola3'
+];
+
+class Drop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: ''
+    }
+  }
+
+  render() {
+    const defaultOption = this.state.selected;
+    return (
+      <div className={styles.DropdownControl}>
+        <Dropdown
+          options={options}
+          onChange={this._onSelect}
+          value={defaultOption}
+          placeholder="Wybierz szkołę"
+          placeholderClassName={styles.MyPlaceholderClassName}
+        />
+        <span className={styles.DropdownArrow}></span>
+      </div>
+    )
+  }
+
+}
+
+export default Drop;
