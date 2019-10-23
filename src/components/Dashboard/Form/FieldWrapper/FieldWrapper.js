0366967d6623c99ab09def64536d0f1fdeb05212
Agata <kozinska.agata@gmail.com>
2019-10-23 19:36:27 +0200

    Add code review corrections

--- src/components/Dashboard/Form/FieldWrapper/FieldWrapper.js
+++ src/components/Dashboard/Form/FieldWrapper/FieldWrapper.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Field } from "react-final-form";
-// import appContent from "../../../../appContent";
 import Error from "./ErrorField/ErrorField";
 import styles from "./FieldWrapper.scss";
 import PropTypes from "prop-types";
@@ -8,8 +7,6 @@ import PropTypes from "prop-types";
 const required = value => (value ? undefined : "Required");
 
 const FieldWrapper = ({ name, componentSize, component, label }) => {
-  // const data = appContent;
-
   return (
     <div className={styles[componentSize]}>
       <label className={styles.Label}>{label ? label : "Test"}</label>
