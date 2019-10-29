b86887840eb373fc2285f1b4de4dfa550d6ed125
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2019-10-29 21:13:30 +0100

    [tests] More tests

--- src/components/Dashboard/Form/Team/TeamMembers/TeamMembers.js
+++ src/components/Dashboard/Form/Team/TeamMembers/TeamMembers.js
@@ -1,46 +1,45 @@
 import React from "react";
-import DocumentForm from "../../DocumentForm";
 import FieldWrapper from "../../FieldWrapper/FieldWrapper";
 import { FieldArray } from "react-final-form-arrays";
 import styles from "./TeamMembers.scss";
 
 const TeamMembers = () => (
-    <FieldArray name="members">
-      {({ fields }) => (
-        <React.Fragment>
-          <button
-            onClick={() => {
-              fields.push({ name: "", function: "" });
-            }}
-          >
-            Dodaj
-          </button>
-          {fields.map((element, index) => (
-            <div key={index} className={styles.TeamMembersInputWrapper}>
-              <FieldWrapper
-                name={`${element}.name`}
-                component="input"
-                componentSize="medium"
-                label={`Członek ${index + 1}`}
-              />
-              <FieldWrapper
-                name={`${element}.function`}
-                component="input"
-                componentSize="medium"
-                label="Specjalizacja"
-              />
-              <button
-                onClick={() => {
-                  fields.remove(index);
-                }}
-              >
-                Usuń
-              </button>
-            </div>
-          ))}
-        </React.Fragment>
-      )}
-    </FieldArray>
+  <FieldArray name="members">
+    {({ fields }) => (
+      <React.Fragment>
+        <button
+          onClick={() => {
+            fields.push({ name: "", function: "" });
+          }}
+        >
+          Dodaj
+        </button>
+        {fields.map((element, index) => (
+          <div key={index} className={styles.TeamMembersInputWrapper}>
+            <FieldWrapper
+              name={`${element}.name`}
+              component="input"
+              componentSize="medium"
+              label={`Członek ${index + 1}`}
+            />
+            <FieldWrapper
+              name={`${element}.function`}
+              component="input"
+              componentSize="medium"
+              label="Specjalizacja"
+            />
+            <button
+              onClick={() => {
+                fields.remove(index);
+              }}
+            >
+              Usuń
+            </button>
+          </div>
+        ))}
+      </React.Fragment>
+    )}
+  </FieldArray>
 );
 
 export default TeamMembers;
--- src/components/Layout.test.js
+++ src/components/Layout.test.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./Layout";
+import Layout from "./Layout.js";
 
 // eslint-disable-next-line no-undef
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Layout />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Layout", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Layout />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
