9f209e8e6dd7af03f646ad4bd22b51ab9404f12f
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2019-10-30 20:11:30 +0100

    [tests] Post review changes

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 
-describe("Sidebar", () => {
+describe("Header", () => {
   it("should be created", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Header />, div);
