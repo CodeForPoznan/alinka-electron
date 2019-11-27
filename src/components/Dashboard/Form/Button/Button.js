88c1769ad95a93ab723543e53102a090661a50f0
paulinatar <p.tarnawska@gmail.com>
2019-11-27 19:02:18 +0100

    first simple test for component

--- src/components/Dashboard/Form/Button/Button.js
+++ src/components/Dashboard/Form/Button/Button.js
@@ -2,24 +2,21 @@ import React from "react";
 import styles from "./Button.scss";
 import PropTypes from "prop-types";
 
-// const Button = props => (
-//   <button type={props.type} className={styles.Button} onClick={props.onClick}>
-//     {props.label}
-//   </button>
-// );
-
-const Button = props => {
-  // console.table(props)
-  console.log(props.onClick, typeof props.onClick)
-  return (<button type={props.type} className={styles.Button} onClick={props.onClick}>
-    {props.label}
-  </button>);
-};
-
-export default Button;
+const Button = props => (
+  <div>
+    <button type={props.type} className={styles.Button} onClick={props.onClick}>
+      {props.label}
+    </button>
+  </div>
+);
 
 Button.propTypes = {
   onClick: PropTypes.func,
   label: PropTypes.string,
   type: PropTypes.oneOf(["button", "submit"])
 };
+
+Button.defaultProps = {
+  type: "button"
+};
+export default Button;
--- src/components/Dashboard/Form/DocumentForm.js
+++ src/components/Dashboard/Form/DocumentForm.js
@@ -70,16 +70,11 @@ export default class DocumentForm extends Component {
             {activePage}
             <div className={styles.buttons}>
               {page > 0 && <Button  onClick={this.previous} label="wróć"/>}
-              {/*{page > 0 && (*/}
-              {/*    <button type="button" onClick={this.previous}>*/}
-              {/*      Wróć2*/}
-              {/*    </button>*/}
-              {/*)}*/}
               {isLastPage ? (
-                  <Button type="submit" disabled={submitting} label="utwórz dokument"/>
+                  <Button  type="submit" disabled={submitting} label="utwórz dokument"/>
 
               ) : (
-                  <Button label="dalej" />
+                  <Button label="dalej"  onClick={(values) =>this.next(values)}/>
               )}
             </div>
 
