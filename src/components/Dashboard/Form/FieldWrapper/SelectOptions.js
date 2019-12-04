fb9d9b91ec67b5772dfbb9f9776f43d2126ec0bb
damianwojtkowski <wojtkowskidamian@gmail.com>
2019-12-04 20:56:39 +0100

    #96 - Added disabled in option list (#104)

--- src/components/Dashboard/Form/FieldWrapper/SelectOptions.js
+++ src/components/Dashboard/Form/FieldWrapper/SelectOptions.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const OptionList = props => {
-  const { name, onChange, options } = props;
+  const { name, onChange, options, disabled } = props;
 
   return (
-    <select name={name} onChange={onChange}>
+    <select name={name} onChange={onChange} disabled={disabled}>
       {options.map(item => (
         <option key={item.key} value={item.value}>
           {item.text}
@@ -18,7 +18,8 @@ const OptionList = props => {
 OptionList.propTypes = {
   onChange: PropTypes.func.isRequired,
   name: PropTypes.string,
-  options: PropTypes.array
+  options: PropTypes.array,
+  disabled: PropTypes.bool
 };
 
 export default OptionList;
