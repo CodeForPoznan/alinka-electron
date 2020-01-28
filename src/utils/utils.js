e810fb09126513af2c8dfaa3def3f8b18d3e9f58
Rafal Kufel <rafal.kufel@gmail.com>
2020-01-28 13:33:56 +0100

    add DocumentData to calculate template data.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,18 +11,86 @@ const disabilityMap = {
   AUTYZM: "z autyzmem, w tym z zespołem Aspergera"
 };
 
-const calculateValues = values => {
-  const reason = values.applicant.reason;
-  let secondReason = values.applicant.secondReason;
-  if (reason && secondReason) {
-    const firstReason = disabilityMap[reason];
-    secondReason = disabilityMap[secondReason];
-    values.applicant.multipleDisability = [firstReason, secondReason];
-    values.applicant.reason = "SPRZEZONA";
+class DocumentData {
+  constructor (values) {
+    this.values = values;
   }
-  return values;
-};
+
+  isMultipleDisability () {
+    return Boolean(this.values.reason && this.values.secondReason)
+  }
+
+  reason () {
+    if (this.isMultipleDisability) {
+      return "SPRZEZONA"
+    }
+    return this.values.applicant.reason
+  }
+
+  multipleDisability () {
+    if (!this.isMultipleDisability) {
+      return []
+    }
+    const firstReasonDescription = disabilityMap[this.values.applicant.reason]
+    const secondReasonDescription = disabilityMap[this.values.applicant.secondReason]
+    return [firstReasonDescription, secondReasonDescription]
+  }
+
+  teamMembers () {
+    return this.values.teamMembers
+  }
+
+  templateData () {
+    return {
+      child: {
+        name: this.values.child.name,
+        pesel: this.values.child.pesel,
+        birthPlace: this.values.child.birthPlace,
+        city: this.values.child.city,
+        postalCode: this.values.child.postalCode,
+        street: this.values.child.street,
+        houseNumber: this.values.child.houseNumber,
+        schoolType: this.values.child.schoolType,
+        schoolName: this.values.child.schoolName,
+        profession: this.values.child.profession,
+        class: this.values.child.class,
+        address: this.values.child.address,
+        birthDate: this.values.child.birthDate
+      },
+      applicant: {
+        address: this.values.applicant.address,
+        name: this.values.applicant.name,
+        city: this.values.applicant.city,
+        postalCode: this.values.applicant.postalCode,
+        street: this.values.applicant.street,
+        houseNumber: this.values.applicant.houseNumber,
+        issue: this.values.applicant.issue,
+        period: this.values.applicant.period,
+        reason: this.values.applicant.reason,
+        multipleDisability: this.multipleDisability()
+      },
+      city: this.values.city,
+      date: this.values.date,
+      no: this.values.no,
+      parents: {
+        address: this.values.parents.address,
+        name: this.values.parents.name
+      },
+      school: this.values.school,
+      supportCenter: {
+        address: this.values.supportCenter.address,
+        members: this.teamMembers(),
+        name: this.values.supportCenter.name,
+        post: this.values.supportCenter.post,
+        city: this.values.supportCenter.city,
+        postalCode: this.values.supportCenter.postalCode,
+        street: this.values.supportCenter.street
+      },
+      kurator: this.values.kurator
+    }
+  }
+}
 
 module.exports = {
-  calculateValues
+  DocumentData
 };
