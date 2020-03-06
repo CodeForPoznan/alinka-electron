6dc9afec46ff72e8e8c149cd8af8be2edb5118d1
FilipCiesielski <filip.carpenter@gmail.com>
2020-03-06 21:14:01 +0100

    changes

--- src/components/Dashboard/ProgressBar/ProgressBar.js
+++ src/components/Dashboard/ProgressBar/ProgressBar.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./ProgressBar.scss";
 
-const ProgressBar = () => {
-  return (
-    <ul className={styles.ProgressBar}>
-      <li>Dane dziecka</li>
-      <li>Wnioskodawcy</li>
-      <li>Zespół</li>
-      <li>Podsumowanie</li>
-    </ul>
-  );
-};
+class ProgressBar extends Component {
+  
+isActive=(stepNumber)=>{
+  if(this.props.pageCounter>=stepNumber){
+    console.log(this.props.pageCounter)
+    return styles.active
+  }
+}
+    render() {
+
+    return (
+      <div className={styles.ProgressBar}>
+      <ul className={styles.List}>
+        <li className={this.isActive(0)} >Dane</li>
+        <li className={this.isActive(1) }>Wnioskodawcy</li>
+        <li className={this.isActive(2) }>Zespół</li>
+       </ul>
+    </div>
+    );
+  }
+}
 
 export default ProgressBar;
