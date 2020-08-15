bee5c94c9e16082f5049b08b6478ab3c9f51152e
Maciej Matuszczak <maciej.matuszczak@stxnext.pl>
2020-08-15 13:08:03 +0200

    Table POC [WIP]

--- src/components/Dashboard/Children/Children.js
+++ src/components/Dashboard/Children/Children.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Button from "../../Button/Button";
 import HeaderIcon from "../../../assets/icons/profile.svg";
 
 import styles from "./Children.scss";
+
 
 const ChildrenHeader = () => (
   <div className={styles.ChildrenHeader}>
@@ -14,6 +16,29 @@ const ChildrenHeader = () => (
   </div>
 );
 
+const ChildrenTable = ({ children }) => (
+  <table className={`${styles.ChildrenTable}`}>
+    <thead>
+        <tr>
+        <th>PESEL</th>
+        <th>Imię</th>
+        <th>Nazwisko</th>
+        <th>Akcje</th>
+        </tr>
+    </thead>
+    {children.map(child => (
+      <tr key={child.PESEL}>
+        <td>{child.PESEL}</td>
+        <td>{child.firstName}</td>
+        <td>{child.lastName}</td>
+        <td>
+            <span>Przygotuj wniosek</span>
+        </td>
+      </tr>
+    ))}
+  </table>
+);
+
 const Children = () => (
   <div className={`${styles.Children}`}>
     <ChildrenHeader />
@@ -21,7 +46,24 @@ const Children = () => (
       <input type="text" name="childName" />
       <Button>Szukaj</Button>
     </div>
+    <ChildrenTable>
+      {[
+        { firstName: "Jan", lastName: "Kowalski", PESEL: "99080255555" },
+        { firstName: "Arnold", lastName: "Biedroń", PESEL: "03211252395" },
+        { firstName: "Szymon", lastName: "Biedraszewski", PESEL: "11320757736" }
+      ]}
+    </ChildrenTable>
   </div>
 );
+
+ChildrenTable.propTypes = {
+  children: PropTypes.arrayOf(
+    PropTypes.shape({
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      PESEL: PropTypes.string
+    })
+  ).isRequired
+};
 
 export default Children;
