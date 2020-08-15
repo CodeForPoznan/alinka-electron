4048782bed420710c909201ca9da8bfbd27f317f
Maciej Matuszczak <maciej.matuszczak@stxnext.pl>
2020-08-15 11:48:16 +0200

    Linter

--- src/components/Dashboard/Children/Children.js
+++ src/components/Dashboard/Children/Children.js
@@ -5,24 +5,23 @@ import HeaderIcon from "../../../assets/icons/profile.svg";
 
 import styles from "./Children.scss";
 
-
 const ChildrenHeader = () => (
-    <div className={styles.ChildrenHeader}>
-        <HeaderIcon />
-        <h2 className={styles.Heading}>
-            <span>Wyszukaj dzieci</span>
-        </h2>
-    </div>  
+  <div className={styles.ChildrenHeader}>
+    <HeaderIcon />
+    <h2 className={styles.Heading}>
+      <span>Wyszukaj dzieci</span>
+    </h2>
+  </div>
 );
 
 const Children = () => (
-    <div className={`${styles.Children}`}>
-        <ChildrenHeader />
-        <div className={`${styles.ChildrenFilterContainer}`}>
-            <input type="text" name="childName" />
-            <Button>Szukaj</Button>
-        </div>
+  <div className={`${styles.Children}`}>
+    <ChildrenHeader />
+    <div className={`${styles.ChildrenFilterContainer}`}>
+      <input type="text" name="childName" />
+      <Button>Szukaj</Button>
     </div>
+  </div>
 );
 
 export default Children;
