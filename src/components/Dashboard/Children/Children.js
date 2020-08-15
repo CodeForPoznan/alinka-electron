1c2247af9d17e12bcc261dad7a94423e15be4fc2
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2020-08-15 13:09:38 +0200

    Added structure for Children tab (#171)
    * Added structure for Children tab
    * Linter

--- src/components/Dashboard/Children/Children.js
+++ src/components/Dashboard/Children/Children.js
@@ -1,5 +1,27 @@
 import React from "react";
 
-const Children = () => <div>Szukajka dzieci</div>;
+import Button from "../../Button/Button";
+import HeaderIcon from "../../../assets/icons/profile.svg";
+
+import styles from "./Children.scss";
+
+const ChildrenHeader = () => (
+  <div className={styles.ChildrenHeader}>
+    <HeaderIcon />
+    <h2 className={styles.Heading}>
+      <span>Wyszukaj dzieci</span>
+    </h2>
+  </div>
+);
+
+const Children = () => (
+  <div className={`${styles.Children}`}>
+    <ChildrenHeader />
+    <div className={`${styles.ChildrenFilterContainer}`}>
+      <input type="text" name="childName" />
+      <Button>Szukaj</Button>
+    </div>
+  </div>
+);
 
 export default Children;
