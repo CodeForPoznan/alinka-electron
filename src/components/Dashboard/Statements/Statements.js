283c7d2adf504b6701f303b9f270624da399b27b
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2020-10-24 15:38:33 +0200

    Added baisc styles for Statements page (#174)

--- src/components/Dashboard/Statements/Statements.js
+++ src/components/Dashboard/Statements/Statements.js
@@ -1,5 +1,22 @@
 import React from "react";
 
-const Statements = () => <div>Szukajka orzeczeń</div>;
+import HeaderIcon from "../../../assets/icons/profile.svg";
+
+import styles from "./Statements.scss";
+
+const StatementsHeader = () => (
+  <div className={styles.StatementsHeader}>
+    <HeaderIcon />
+    <h2 className={styles.Heading}>
+      <span>Wyszukaj orzeczenia</span>
+    </h2>
+  </div>
+);
+
+const Statements = () => (
+  <div className={styles.Statements}>
+    <StatementsHeader />
+  </div>
+);
 
 export default Statements;
